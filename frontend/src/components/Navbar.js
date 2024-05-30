03f96adb4691a3e0324716d49e6d186f1d7cea1e
TrimNCut <daviestise@gmail.com>
2024-05-30 22:47:41 +0100

    Updated Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
+import Arrow from "../images/caret-down-solid.svg"
 
 export default function Navbar() {
     return(
         <nav>
-            <h1><Link to="/">EasyTutor</Link></h1>
+            <h1><Link to="/">USI Tutors</Link></h1>
             <Link>Home</Link>
             <Link>Our Mission</Link>
-            <Link>Find a Tutor</Link>
+            <Link>Find a Tutor<img alt="arrow-down" src={Arrow}/></Link>
             <button class="outlineBtn">SignUp</button>
             <hr/>
             <button class="fullBtn">Login</button>
