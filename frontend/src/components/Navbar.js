676f26f07768153f01f2471c24e8cab92e36c71b
IBK <165066068+IBK2024@users.noreply.github.com>
2024-05-30 22:05:31 +0100

    feat(backend): set up node js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    return(
-        <nav>
-            <h1><Link to="/">EasyTutor</Link></h1>
-            <Link>Home</Link>
-            <Link>Our Mission</Link>
-            <Link>Find a Tutor</Link>
-            <button class="outlineBtn">SignUp</button>
-            <hr/>
-            <button class="fullBtn">Login</button>
-        </nav>
-    )
-};
+  return (
+    <nav>
+      <h1>
+        <Link to="/">EasyTutor</Link>
+      </h1>
+      <Link>Home</Link>
+      <Link>Our Mission</Link>
+      <Link>Find a Tutor</Link>
+      <button class="outlineBtn">SignUp</button>
+      <hr />
+      <button class="fullBtn">Login</button>
+    </nav>
+  );
+}
