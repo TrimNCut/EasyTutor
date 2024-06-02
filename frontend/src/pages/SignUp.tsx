63b69b51d184575f9914445790d81fdbae3c44d2
SwordKingIBK <ibukunoluwa.adetoba@gmail.com>
2024-06-02 16:59:12 +0100

    refactor: changes to pre-push commit hook

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,8 +1,8 @@
 export default function SignUp() {
   return (
     <div className='authcon'>
-      <div className='authconleft'></div>
-      <div className='authconright'></div>
+      <div className='authconleft' />
+      <div className='authconright' />
     </div>
   );
 }
