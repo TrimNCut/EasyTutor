aedfc4129b71746f030a95238c1653833698a4ad
TrimNCut <daviestise@gmail.com>
2024-06-04 02:59:46 +0700

    Signup updated

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -8,21 +8,29 @@ export default function SignUp() {
         <h1>Sign Up Today!</h1>
         <hr />
         <div className='authcondiv'>
-          <img src='/envelope-solid.svg' alt='envelope' />
+          <img src='/envelope-solid.svg' alt='envelopeicon' className='authconsideimg' />
           <input type='email' placeholder='Your Mail' />
         </div>
         <div className='authcondiv'>
+          <img src='/user-solid-dark.svg' alt='usericon' className='authconsideimg' />
           <input type='text' placeholder='Username' />
         </div>
         <div className='authcondiv'>
+          <img src='/key-solid.svg' alt='usericon' className='authconsideimg' />
           <input type='text' placeholder='Password' />
+        </div>
+        <div className='authcondiv'>
+          <img src='/key-solid.svg' alt='usericon' className='authconsideimg' />
+          <input type='text' placeholder='Confirm Password' />
         </div>
         <p>Register as:</p>
         <div className='authcondiv'>
           <button className='authconoptions authparents' type='button'>
             <img src='/user-solid.svg' alt='For Parents' />
           </button>
-          <button className='authconoptions authteachers' type='button' />
+          <button className='authconoptions authteachers' type='button'>
+            <img src='/chalkboard-user-solid.svg' alt='For Parents' />
+          </button>
         </div>
         <div className='authcondiv'>
           <button className='authconsubmit' type='submit'>
